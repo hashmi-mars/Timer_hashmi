43a17c19f0a8019a62821b9ace46fbe3886f396d
AbuBakar <hashmi.mars@gmail.com>
2022-02-11 15:06:22 +0500

    quick_Update
    Timer is working fine

--- Hashmi_Timer.py
+++ Hashmi_Timer.py
@@ -1,16 +1,16 @@
 import os
 import time
 
-seconds = float(0)
-minutes = int(0)
 hours = int(0)
+minutes = int(0)
+seconds = float(0)
 
 run = input("Enter R to run the program.")
 
 while run.lower()=="r":
 	if seconds > 59:
 		seconds = 0
-		minutes = minutes+1
+		minutes = minutes+1r
 	if minutes > 59:
 		minutes = 0
 		hours = hours+1
